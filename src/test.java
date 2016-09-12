666a9f6612bc7275617ed4672c0ec66991808496
40329 <40329@RMH171332.psdschools.org>
2016-09-12 12:59:40 -0600

    Finished project

--- src/test.java
+++ src/test.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter an integer:");
+		x = sc.nextInt();
+		if ((x & 1) == 0) {
+			System.out.println("Even");
+		} else {
+			System.out.println("Odd");
+		}
 
 	}
 
